74cb69b5920ebdc2a3a6a764c5be2691f325dae6
KhanhDang <emailcuadang1705@gmail.com>
2026-05-15 21:17:48 +0700

    first commit

---
+++ src/main/java/vn/work/exe/exe_project/ExeProjectApplication.java
@@ -0,0 +1,13 @@
+package vn.work.exe.exe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/work/exe/exe_project/ExeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.work.exe.exe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
